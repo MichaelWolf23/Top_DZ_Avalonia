2c7103d3b88a7ce8ab9d8101acc1456ea51ce694
Михаил <michaelwolf23@yandex.ru>
2026-02-03 17:15:14 +0300

    Домашнее задание 3

---
+++ Top_DZ3_Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Top_DZ3_Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
